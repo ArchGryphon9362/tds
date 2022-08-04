f36e741bac9a28edc8136e570d88284f701d65da
Lex Nastin <lexnastin@gmail.com>
2022-08-04 01:12:31 +0100

    Clean Up

--- build/tonccpy.d
+++
@@ -1,4 +0,0 @@
-tonccpy.o: /home/lex/tds/source/tonccpy.c /home/lex/tds/source/tonccpy.h \
- /opt/devkitpro/libnds/include/nds/ndstypes.h
-/home/lex/tds/source/tonccpy.h:
-/opt/devkitpro/libnds/include/nds/ndstypes.h:
